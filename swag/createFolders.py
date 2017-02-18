fa4fdaafb19d43127e1c22374bf91e418edccc81
ransomware <mohamed.tahrioui@wheregroup.com>
2017-02-18 22:26:17 +0100

    Improve installation process and usage

--- swag/createFolders.py
+++ swag/createFolders.py
@@ -4,15 +4,20 @@ import os
 import colors
 
 
-directory = os.path.expanduser("~")+"/.colors"
-colorDictionary= colors.COLORS
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
+def install(dest = "default"):
+    if dest == "default":
+        directory = os.path.expanduser("~") + "/.colors"
+    else :
+        directory = dest
 
+    colorDictionary = colors.COLORS
 
-for i, key in enumerate(colorDictionary):
-    print key
-    file = open( directory+"/"+key+".txt" , "w" )
-    file.write( colorDictionary[key] )
-    file.close()
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for i, key in enumerate(colorDictionary):
+        print colors.COLORS["reset"] + colors.COLORS[key] + "Installed "+ directory + "/"+ key + colors.COLORS["reset"]
+        file = open(directory + "/" + key, "w")
+        file.write(colorDictionary[key])
+        file.close()
--- swag/swag.py
+++ swag/swag.py
@@ -11,15 +11,28 @@ directory = os.path.expanduser("~") + "/.colors"
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-c", "--color", default="white",
-                    help="default = white - which color to format the text in : black|red|green|yellow|blue|purple|cyan|white")
+subParsers = parser.add_subparsers(help='[command] help')
 
-parser.add_argument("-t", "--type", default="normal",
-                    help="default = normal - format the string with either: normal|underline|background|bold|intense|intenseBold|intenseBackground")
+installParser = subParsers.add_parser("install",
+                                      help="install the colors to the folder of choice")
 
-parser.add_argument("text", type=str, help="the text to prettyEcho")
+installParser.add_argument_group()
+installParser.add_argument("-d", "--dest", default="default",
+                           help="default = ~/.colors - install the color escape sequences to the specified destination")
 
+installParser.add_argument("-l", "--list", default=False, action="store_true")
 
+printParser = subParsers.add_parser("print",
+                                    help="prints the text with the specified color and type to the console")
+
+printParser.add_argument_group()
+printParser.add_argument("-c", "--color", default="white",
+                         help="default = white - which color to format the text in : black|red|green|yellow|blue|purple|cyan|white")
+
+printParser.add_argument("-t", "--type", default="normal",
+                         help="default = normal - format the string with either: normal|underline|background|bold|intense|intenseBold|intenseBackground")
+
+printParser.add_argument("text", type=str, default="", help="the text to prettyEcho")
 
 
 def getKey(color, type):
@@ -38,17 +51,33 @@ def getKey(color, type):
         return "white"
 
 
+def install(dest):
+    import createFolders
+    createFolders.install(dest)
+
+
+def list():
+    colorDictionary = colors.COLORS
+    for i, key in enumerate(colorDictionary):
+        print colors.COLORS["reset"]+ colors.COLORS[key]+ key+colors.COLORS["reset"]
+
+
 def main():
-
     args = parser.parse_args()
-    color = args.color
-    type = args.type
-    text = args.text
 
-    key = getKey(color, type)
-    colorMod = colors.COLORS[key]
-
-    print colorMod + text
+    if "dest" in args:
+        dest = args.dest
+        if not args.list:
+            install(dest)
+        else:
+            list()
+    else:
+        color = args.color
+        type = args.type
+        text = args.text
+        key = getKey(color, type)
+        colorMod = colors.COLORS[key]
+        print colorMod + text
 
 
 if __name__ == '__main__':
