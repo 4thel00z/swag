1b4bc69de371f9ed04c2469aea3258d04742bc26
ransomware <4thel00z@gmail.com>
2017-09-18 19:57:25 +0200

    Add python 2.6 legacy formatting style. Fix modifier parameter usage

--- swag/swagprinter.py
+++ swag/swagprinter.py
@@ -18,42 +18,42 @@ INTENSE_BACKGROUND = "intenseBackground"
 def _print_swaggy(color, content, modifier=None, continous=False):
     if modifier is not None:
         color = modifier + color.title()
-    text = "{}{}".format(color, content) \
-        if continous else "{}{}{}".format(color, content, colors.COLORS["reset"])
+    text = "{0}{1}".format(color, content) \
+        if continous else "{0}{1}{2}".format(color, content, colors.COLORS["reset"])
 
     print(text)
 
 
 def print_black(content, modifier=None, continous=False):
-    _print_swaggy(colors.COLORS["black"], content, continous)
+    _print_swaggy(colors.COLORS["black"], content, modifier, continous)
 
 
 def print_yellow(content, modifier=None, continous=False):
-    _print_swaggy(colors.COLORS["yellow"], content, continous)
+    _print_swaggy(colors.COLORS["yellow"], content, modifier, continous)
 
 
 def print_green(content, modifier=None, continous=False):
-    _print_swaggy(colors.COLORS["green"], content, continous)
+    _print_swaggy(colors.COLORS["green"], content, modifier, continous)
 
 
 def print_red(content, modifier=None, continous=False):
-    _print_swaggy(colors.COLORS["red"], content, continous)
+    _print_swaggy(colors.COLORS["red"], content, modifier, continous)
 
 
 def print_blue(content, modifier=None, continous=False):
-    _print_swaggy(colors.COLORS["blue"], content, continous)
+    _print_swaggy(colors.COLORS["blue"], content, modifier, continous)
 
 
 def print_purple(content, modifier=None, continous=False):
-    _print_swaggy(colors.COLORS["purple"], content, continous)
+    _print_swaggy(colors.COLORS["purple"], content, modifier, continous)
 
 
 def print_white(content, modifier=None, continous=False):
-    _print_swaggy(colors.COLORS["white"], content, continous)
+    _print_swaggy(colors.COLORS["white"], content, modifier, continous)
 
 
 def print_cyan(content, modifier=None, continous=False):
-    _print_swaggy(colors.COLORS["cyan"], content, continous)
+    _print_swaggy(colors.COLORS["cyan"], content, modifier, continous)
 
 
 def reset():
